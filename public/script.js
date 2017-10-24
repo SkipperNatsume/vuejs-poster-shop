faabdc615b485a48271cae9d0ee75b8df01cdf29
Nguyen Cong Danh <no1st.skipper@gmail.com>
2017-10-24 17:09:46 +0300

    S7L44
    Finished at fading transition, with minor problems

--- public/script.js
+++ public/script.js
@@ -2,38 +2,17 @@ new Vue({
     el: '#app',
     data: {
         total: 0,
-        items: [{
-            id: 1,
-            title: 'Item 1',
-            price: 10
-        },
-        {
-            id: 2,
-            title: 'Item 2',
-            price: 11.11
-        },
-        {
-            id: 3,
-            title: 'Item 3',
-            price: 12.22
-        },
-        {
-            id: 4,
-            title: 'Item 4',
-            price: 13.33
-        },
-        {
-            id: 5,
-            title: 'Item 5',
-            price: 14.44
-        }
-        ],
+        items: [],
         cart: [],
-        search: ' '
+        newSearch: 'Anime',
+        lastSearch: '',
+        searching: false,
+        searched: false,
+        price: 5
     },
     methods: {
         addItem: function (index) {
-            this.total = 0;
+            //this.total = 0;
             var item = this.items[index];
             var found = false;
             for (var i = 0; i < this.cart.length; i++) {
@@ -47,21 +26,22 @@ new Vue({
                 this.cart.push({
                     id: item.id,
                     title: item.title,
-                    price: item.price,
+                    price: this.price,
                     quantity: 1
                 });
             }
-            for (var i = 0; i < this.cart.length; i++) {
+            this.total += this.price;
+            /*for (var u = 0; u < this.cart.length; u++) {
                 this.total += this.cart[i].price * this.cart[i].quantity;
-            }
+            }*/
         },
         inc: function (product) {
             product.quantity++;
-            this.total += product.price;
+            this.total += this.price;
         },
         dec: function (product) {
             product.quantity--;
-            this.total -= product.price;
+            this.total -= this.price;
             if (product.quantity <= 0) {
                 for (var i = 0; this.cart.length; i++) {
                     if (this.cart[i].id === product.id) {
@@ -71,13 +51,25 @@ new Vue({
                 }
             }
         },
-        onSubmit: function(){
-            console.log(this.search);
+        onSubmit: function () {
+            this.items = [];
+            this.searching = true;
+            this.$http.get('/search/'
+                .concat(this.newSearch))
+                .then(function (res) {
+                    this.items = res.data;
+                    this.searching = false;
+                });
+            this.lastSearch = this.newSearch;
+            this.searched = true;
         }
     },
     filters: {
         currency: function (price) {
             return '$'.concat(price.toFixed(2));
         }
+    },
+    mounted: function () {
+        this.onSubmit();
     }
 });
